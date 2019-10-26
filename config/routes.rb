79ac58fbe922adf95b5b33f1e531df8688a220be
17043003 <35760051+17043003@users.noreply.github.com>
2019-10-26 19:25:58 +0900

    change route to index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'information/index'
-  get 'information/show'
+  # get 'information/index'
+  root 'information#index'
+  get 'show' => 'information#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
