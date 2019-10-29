f8b4bf684c298b929017c442dcda0e1b390764b9
17043003 <35760051+17043003@users.noreply.github.com>
2019-10-29 21:27:17 +0900

    create weather model and DB

--- db/migrate/20191029120859_create_weathers.rb
+++ db/migrate/20191029120859_create_weathers.rb
@@ -1,6 +1,9 @@
 class CreateWeathers < ActiveRecord::Migration[5.2]
   def change
     create_table :weathers do |t|
+      t.string :weather, null: false
+      t.date :date, null: false
+      t.string :place, null: false
 
       t.timestamps
     end
